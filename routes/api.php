f4cf2dd3a1e9647a0ddd77af956c81fb59a247c9
Paco <paco@mastermind.ac>
2025-03-18 08:37:21 +0100

    Challenge1 solution

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlayerController;
 
+use App\Http\Middleware\ApiForceAcceptHeader;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
 //Endpoint listado de players
-Route::get('/players', [PlayerController::class, 'index']);
+Route::get('/players', [PlayerController::class, 'index'])->middleware([ApiForceAcceptHeader::class]);
+
+//Endpoint listado de teams
+Route::get('/teams', [PlayerController::class, 'index'])->middleware([ApiForceAcceptHeader::class]);
